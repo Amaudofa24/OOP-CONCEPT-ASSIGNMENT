071dd2811bb0fdf8daac6923c2d764900670e0c7
Support <Support@WEMA-TRAINING-S>
2022-08-08 15:22:14 +0100

    Add project files.

---
+++ ConsoleApp5/FullTimeEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spleet
+{
+    public abstract class FullTimeEmployee
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int AnnualSalary { get; set; }
+        public abstract string GetFullName(string firstName, string lastName);
+        
+        
+        public int GetMonthlySalary() { return AnnualSalary / 12; }
+
+
+    }
+    public class PartTimeEmployee : FullTimeEmployee
+    {
+
+        public int hourlyPay { get; set; }
+        public int totalHours { get; set; }
+
+        public override string GetFullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+                    }
+
+        public  string GetFullName(string firstName, string lastName,string middleName)
+        {
+            return firstName + " " + lastName;
+        }
+
+
+        public int payment { get; set; }
+        public int GetPayment() { return totalHours * hourlyPay; }
+    }
+}
+  
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Spleet
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            PartTimeEmployee pte = new PartTimeEmployee();
+            pte.ID = 205;
+            pte.firstName = "Ama";
+            pte.lastName = "Tasha";
+            pte.AnnualSalary = 150000;
+            
+            Console.WriteLine(pte.GetFullName(pte.firstName,pte.lastName));
+            Console.WriteLine(pte.GetMonthlySalary());
+
+          
+            pte.hourlyPay = 1000;
+            pte.totalHours = 40;
+            pte.payment = 40000;
+            Console.WriteLine(pte.GetMonthlySalary());
+          }  
+        
+            
+    }
+}
+            
+
+
+        
+            
+
+    
+
+
+
